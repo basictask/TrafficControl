519ec63a00912a34c7b46726a8e6be3a0a0ce083
basictask <daniel.kuknyo@mailbox.org>
2022-11-04 16:02:21 +0100

    added graphs

--- _test_reader.py
+++ _test_reader.py
@@ -7,14 +7,14 @@ from city_constructor import *
     
 #%% Set up the reader from a .html GeoGebra construction protocol
 
-filepath = 'cities/simple.html'
+# filepath = 'cities/simple.html'
 # filepath = 'cities/starcity.html'
-# filepath = 'cities/bakats.html'
+filepath = 'cities/bakats.html'
 
 # Points that are valid for entering the traffic system
-entry_points = ['A','C','E','G'] # Simple
+# entry_points = ['A','C','E','G'] # Simple
 # entry_points = ['A','D','F','H','J'] # Star city
-# entry_points = ['P','O','Q','M','K','J','T','S','R','G','E','F'] # Bakats area 
+entry_points = ['P','O','Q','M','K','J','T','S','R','G','E','F'] # Bakats area 
 
 vrate = 60 # Rate of vehicles coming in from each entry point
 paths_to_gen = 10 # How many paths to generate
@@ -23,8 +23,8 @@ steps_per_update = 5 # How many steps the game takes in the interval of one fram
 
 r = reader(filepath, entry_points, vrate, paths_to_gen, path_dist)
 
-# roads, vehicle_mtx = r.get_matrices()
-# start_sim(roads, vehicle_mtx, (-150, -110), steps_per_update)
+roads, vehicle_mtx = r.get_matrices()
+start_sim(roads, vehicle_mtx, (-150, -110), steps_per_update)
 
 #%% Tessting add function
 
---
+++ doc/graphs/render.py
@@ -0,0 +1,23 @@
+"""
+This is a small script to render all dot files in this folder into png files
+Input: a folder path
+Output: png files from each dot file
+"""
+
+import os
+import pydot
+
+target_dir = '/home/daniel/Documents/ELTE/trafficControl/doc/graphs'
+os.chdir(target_dir)
+
+for file in os.listdir(target_dir):
+    if(file.endswith('.dot')):
+        print("Rendering: " + file + "... ", end = "") 
+        
+        (graph, ) = pydot.graph_from_dot_file(file)
+        name = file.split('.')[0]
+        graph.write_png(name + '.png')
+        
+        print("Success")
+
+print("Done")
