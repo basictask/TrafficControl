86edebecd86a9d824dc6d262294c90c4d70ee29a
basictask <daniel.kuknyo@mailbox.org>
2022-11-07 20:44:57 +0100

    some minor changes

--- _test_reader.py
+++ _test_reader.py
@@ -8,14 +8,15 @@ There are predefined cities and entry points added as comments.
 
 # Imports
 import os
+
 os.chdir('/home/daniel/Documents/ELTE/trafficControl')
 
 from trafficSimulator import *
 from city_constructor import *
 
-test_add = False # Modifying this to True will result in testing the add/remove functions of the reader class
+test_add = False  # Modifying this to True will result in testing the add/remove functions of the reader class
 
-#%% Set up the reader from a .html GeoGebra construction protocol
+# %% Set up the reader from a .html GeoGebra construction protocol
 
 # filepath = 'cities/Simple.html'
 # filepath = 'cities/starcity.html'
@@ -24,22 +25,22 @@ filepath = 'cities/bakats.html'
 # Points that are valid for entering the traffic system
 # entry_points = ['A','C','G','J'] # Simple
 # entry_points = ['A','D','F','H','J'] # Star city
-entry_points = ['A', 'M', 'E', 'K', 'J', 'I', 'B', 'F', 'C', 'D', 'T'] # Bakats area 
+entry_points = ['A', 'M', 'E', 'K', 'J', 'I', 'B', 'F', 'C', 'D', 'T']  # Bakats area
 
-vrate = 60 # Rate of vehicles coming in from each entry point
-paths_to_gen = 10 # How many paths to generate
-path_dist = 'normal' # One of 'normal', 'uniform' 
-steps_per_update = 5 # How many steps the game takes in the interval of one frame update
+vrate = 60  # Rate of vehicles coming in from each entry point
+paths_to_gen = 10  # How many paths to generate
+path_dist = 'normal'  # One of 'normal', 'uniform'
+steps_per_update = 5  # How many steps the game takes in the interval of one frame update
 
 r = reader(filepath, entry_points, vrate, paths_to_gen, path_dist)
 
-if(not test_add):
+if (not test_add):
     roads, vehicle_mtx = r.get_matrices()
     start_sim(roads, vehicle_mtx, (-150, -110), steps_per_update)
 
-#%% Tessting add function
+# %% Tessting add function
 
-if(test_add):
+if (test_add):
     r.add_segment('E', 'G')
     r.add_segment('D', 'H')
     r.add_segment('H', 'D')
@@ -51,7 +52,7 @@ if(test_add):
     # roads, vehicle_mtx = r.get_matrices()
     # start_sim(roads, vehicle_mtx, (-150, -110), steps_per_update)
 
-    #%% Testing remove function
+    # %% Testing remove function
 
     r.remove_segment('B', 'H')
     r.remove_segment('H', 'B')
@@ -59,6 +60,5 @@ if(test_add):
 
     roads, vehicle_mtx = r.get_matrices()
 
-    #%%
+    # %%
     start_sim(roads, vehicle_mtx, (-150, -110), steps_per_update)
-
