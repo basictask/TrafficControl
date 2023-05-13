0be32ae4ecc447d635d85452f870dd08eae5688f
daniel <daniel.kuknyo@mailbox.org>
2023-05-13 11:47:51 +0200

    Finalized documentation

--- doc/render.sh
+++ doc/render.sh
@@ -2,4 +2,4 @@
 
 cp ./latex/elteikthesis_en.pdf .
 
-mv ./elteikthesis_en.pdf ./"Daniel Kuknyo - Traffic Control and Infrestructure Organization Using Reinforcement Learning"
+mv ./elteikthesis_en.pdf ./"Daniel Kuknyo - Traffic Control and Infrestructure Organization Using Reinforcement Learning.pdf"
